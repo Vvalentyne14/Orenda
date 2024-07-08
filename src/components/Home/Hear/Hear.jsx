2c048759795ace7cd2c8276f4df8a5116ab78f17
Valentine <valentineojienor14@gmail.com>
2024-07-08 17:25:23 +0100

    changes as at 7th July

--- src/components/Home/Hear/Hear.jsx
+++ src/components/Home/Hear/Hear.jsx
@@ -8,6 +8,41 @@ import "swiper/css/navigation";
 import quote from "../../../assets/quote.png"
 
 const Hear = () => {
+  const clientsReviews = [
+    {
+      name: "Vanessa E.",
+      initials: "VE",
+      review:`“The staff at this location is amazing! The providers are
+                  great, easy to schedule with and extremely accommodating! They
+                  are so helpful in trying to make sure all of your needs are
+                  met and everything goes smoothly in addition to being so
+                  responsive and supportive. I also have to mention how great
+                  the women who answer the phones are! Everyone I’ve interacted
+                  with at this practice has been so kind and helpful. I highly,
+                  highly recommend!!”`
+    },
+    {
+      name: "Deb G.",
+      initials: "DG",
+      review:`"This place is really great. I have been with the same practitioner for years now and when she started at this practice I was really impressed by their efficiency. Highly recommend. If you get to see Bethany, she is a fantastic practitioner and really knows her stuff. Couldn’t recommend her more"`
+    },
+    {
+      name: "Bela M.",
+      initials: "BM",
+      review:` "I was so very happy to get connected to my provider through their team. I consider myself lucky that I am receiving the right care and attention. Their admin is also very professional and thorough."`
+    },
+    {
+      name: "Kimnerly M.",
+      initials: "KM",
+      review:` "I am currently working with Danielle Overton and she is very professional, kind, compassionate and is a great listener. She has helped me a lot during this tough time in my life. I would highly recommend Ms. Overton and Orenda Psychiatry! They have helped me tremendously!"`
+    },
+    {
+      name: "Collette A",
+      initials: "CA",
+      review:`"Shelley is an incredible psychiatrist who has had a profoundly positive impact on my life. From our very first session I felt understood and supported in ways I hadn't experienced before. She was able to refine my diagnosis so my treatment plan could be more tailored to my specific needs, and she uncovered another diagnosis that had been missed by countless previous doctors. Shelley’s empathetic in her approach, listens attentively, and offers invaluable insights."`
+    },
+  ]
+
   return (
     <div className="hear-container">
       <div className="px-0">
@@ -15,50 +50,23 @@ const Hear = () => {
         <div className="px-4">
         <div className="max-w-[50.13rem] mx-auto relative">
         <img src={quote} alt="" className="absolute -left-[.8rem] -top-[3.4rem] ~size-10/[4rem]" />
-          <Swiper modules={[Navigation, Pagination]}>
+          <Swiper modules={[Navigation, Pagination]} autoHeight = {true}>
          
-            <SwiperSlide className="relative sslide">
+            {clientsReviews.map(client => (<SwiperSlide className="relative sslide">
               <div className=" grid gap-6 font-dm-sans text-[18px] leading-[26px]">
                 <p>
-                  “The staff at this location is amazing! The providers are
-                  great, easy to schedule with and extremely accommodating! They
-                  are so helpful in trying to make sure all of your needs are
-                  met and everything goes smoothly in addition to being so
-                  responsive and supportive. I also have to mention how great
-                  the women who answer the phones are! Everyone I’ve interacted
-                  with at this practice has been so kind and helpful. I highly,
-                  highly recommend!!”
+                 {client.review}
                 </p>
               </div>
               <div className=" bg-[rgb(239,235,223)]  mx-auto mt-6 mb-4">
                 <hr className="w-full" />
               </div>
               <div className="flex  items-center gap-4 ">
-                <div className="initials font-open-sans bg-white">VE</div>
-                <p className="font-open-sans font-bold ~text-[1.2rem]/[1.66rem]">Vanessa E.</p>
+                <div className="initials font-open-sans bg-white">{client.initials}</div>
+                <p className="font-open-sans font-bold ~text-[1.2rem]/[1.66rem]">{client.name}</p>
               </div>
-            </SwiperSlide>
-            <SwiperSlide className="relative sslide">
-              <div className="leading-[26px] grid gap-6 font-dm-sans text-[18px]">
-                <p>
-                  “The staff at this location is amazing! The providers are
-                  great, easy to schedule with and extremely accommodating! They
-                  are so helpful in trying to make sure all of your needs are
-                  met and everything goes smoothly in addition to being so
-                  responsive and supportive. I also have to mention how great
-                  the women who answer the phones are! Everyone I’ve interacted
-                  with at this practice has been so kind and helpful. I highly,
-                  highly recommend!!”
-                </p>
-              </div>
-              <div className=" bg-[rgb(239,235,223)]  mx-auto mt-6 mb-4 ">
-                <hr className="w-full"  />
-              </div>
-              <div className="flex  items-center gap-4">
-                <div className="initials fon bg-white">VE</div>
-                <p className="font-open-sans font-bold ~text-[0.69rem]/[1.66rem]">Vanessa E.</p>
-              </div>
-            </SwiperSlide>
+            </SwiperSlide>))}
+            
 
             <ReviewsNavigation />
           </Swiper>
--- src/components/Network/Network.jsx
+++ src/components/Network/Network.jsx
@@ -50,17 +50,17 @@ const Network = () => {
           <h4>Out of network</h4>
         </div>
         <div className={Style.nets}>
-          <img src={emblem} alt="" />
-          <img src={cms} alt="" style={{padding: 8}} />
-          <img src={ghi} alt="" />
-          <img src={metro} alt="" />
+          <img src={emblem} alt=""  className="max-w-[10rem]"/>
+          <img src={cms} alt="" style={{padding: 8}}  className="max-w-[3rem]"/>
+          <img src={ghi} alt=""  className="max-w-[3rem]"/>
+          <img src={metro} alt=""  className="max-w-[10rem]"/>
         </div>
         <div className={Style.netss}>
           <Marquee className={Style.marquee_main_mob} play={true} speed={150}>
-            <div className="marq" style={{paddingRight: "3em"}}><img src={metro} alt="" /></div>
-            <div className="marq" style={{paddingRight: "3em"}}><img src={emblem} alt="" /></div>
-            <div className="marq" style={{paddingRight: "3em"}}><img src={cms} alt="" /></div>
-            <div className="marq" style={{paddingRight: "3em"}}><img src={ghi} alt="" /> </div>
+            <div className="marq" style={{paddingRight: "3em"}}><img src={metro} alt="" className="max-w-[10rem]"/></div>
+            <div className="marq" style={{paddingRight: "3em"}}><img src={emblem} alt="" className="max-w-[10rem]"/></div>
+            <div className="marq" style={{paddingRight: "3em"}}><img src={cms} alt="" className="max-w-[3rem]"/></div>
+            <div className="marq" style={{paddingRight: "3em"}}><img src={ghi} alt="" className="max-w-[3rem]"/> </div>
           </Marquee>
         </div>
       </div>
