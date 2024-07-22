5cd537b8344e6f0986ea8b8b54c6004920827e26
Valentine <valentineojienor14@gmail.com>
2024-07-22 18:09:55 +0100

    last push for 22nd july 24

--- src/components/Home/Available/Available.jsx
+++ src/components/Home/Available/Available.jsx
@@ -16,21 +16,25 @@ const Available = () => {
         <h1>Available Locations</h1>
         <p>We currently serve in these 4 states</p>
         <div className="avail-img">
-        <picture>
-            <source media="(min-width: 451px)" srcset={newyork} />
+          <picture className="relative">
+            <source media="(min-width: 451px)" srcset={newyorkmob} />
             <img src={newyorkmob} alt="newyork" />
+            <span className="absolute left-0 bg-white bg-opacity-70 bottom-[6%] rounded-r-[0.47rem] font-bold ~ps-[0.62rem]/4 ~w-[8.37rem]/[9.6rem] py-2 text-black ~text-base/lg font-dm-sans">New York</span>
           </picture>
-          <picture>
-            <source media="(min-width: 451px)" srcset={connecticut} />
+          <picture className="relative">
+            <source media="(min-width: 451px)" srcset={connecticutmob} />
             <img src={connecticutmob} alt="connecticut" />
+            <span className="absolute left-0 bg-white bg-opacity-70 bottom-[6%] rounded-r-[0.47rem] font-bold ~ps-[0.62rem]/4 ~w-[8.37rem]/[9.6rem] py-2 text-black ~text-base/lg font-dm-sans ">Connecticut</span>
           </picture>
-          <picture>
-            <source media="(min-width: 451px)" srcset={newjersey} />
+          <picture className="relative">
+            <source media="(min-width: 451px)" srcset={newjerseymob} />
             <img src={newjerseymob} alt=" New Jersey" />
+            <span className="absolute left-0 bg-white bg-opacity-70 bottom-[6%] rounded-r-[0.47rem] font-bold ~ps-[0.62rem]/4 ~w-[8.37rem]/[9.6rem] py-2 text-black ~text-base/lg font-dm-sans">New Jersey</span>
           </picture>
-          <picture>
-            <source media="(min-width: 451px)" srcset={massachusetts} />
-            <img src={massachusettsmob} alt="Massachusetts" />  
+          <picture className="relative">
+            <source media="(min-width: 451px)" srcset={massachusettsmob} />
+            <img src={massachusettsmob} alt="Massachusetts" />
+            <span className="absolute left-0 bg-white bg-opacity-70 bottom-[6%] rounded-r-[0.47rem] font-bold ~ps-[0.62rem]/4 ~w-[8.6rem]/[9.6rem] py-2 pr-20 text-black ~text-base/lg font-dm-sans">Massachusetts</span>
           </picture>
         </div>
       </div>
